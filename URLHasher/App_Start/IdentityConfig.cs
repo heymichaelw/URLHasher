58dd7020a0b292755037809a4676aebb5ea53759
heymichaelw <heymichaelw@gmail.com>
2017-02-13 12:39:27 -0500

    added username to registration and login

--- URLHasher/App_Start/IdentityConfig.cs
+++ URLHasher/App_Start/IdentityConfig.cs
@@ -54,14 +54,9 @@ namespace URLHasher
             manager.PasswordValidator = new PasswordValidator
             {
                 RequiredLength = 6,
-                RequireNonLetterOrDigit = true,
-                RequireDigit = true,
-                RequireLowercase = true,
-                RequireUppercase = true,
             };
 
             // Configure user lockout defaults
-            manager.UserLockoutEnabledByDefault = true;
             manager.DefaultAccountLockoutTimeSpan = TimeSpan.FromMinutes(5);
             manager.MaxFailedAccessAttemptsBeforeLockout = 5;
 
